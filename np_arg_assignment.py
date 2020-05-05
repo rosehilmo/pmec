bf2e33f80b531c6cd4c0319c63274cb8cca560df
Rose Hilmo <wader@uw.edu>
2020-05-04 22:42:00 -0700

    comments

--- np_arg_assignment.py
+++ np_arg_assignment.py
@@ -6,13 +6,24 @@ import argparse
 import os
 from os import path
 
+"""
+Call from command line Example: "python3 np_arg_assignment.py -x 3 5 2 4 6 3 -y 6 -v add"
+Can be any combination of numbers after -x
+Can be any integer after -y
+Can be one of 5 choices after -v, add, subtract, multiply, divide, or exponent
+   
+"""
+
 outputfolder='np_arg_assignment_output'
 
+#makes folder if does not exist
 if path.exists(outputfolder) == False:
     os.mkdir(outputfolder)
 
 filename='results.pickle'
 outfile=open(outputfolder + '/' + filename,'wb')
+
+#Defines argparse arguments
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-x','--x',nargs='+',type=int,help='<Required> Set flag', required=True)
@@ -21,7 +32,7 @@ parser.add_argument("-v","--verbosity", type=str,
         choices=['add','subtract','multiply','divide','exponent'],help='choose operand')
 args=parser.parse_args()
 
-#import pdb; pdb.set_trace()
+#Runs whichever argument chosen in argparse arguments
 if args.verbosity == 'add':
     answer=np.add(np.array(args.x),args.y)
     print('Elementwise sum equals' + str(answer))
@@ -43,6 +54,6 @@ if args.verbosity == 'exponent':
     print('Elementwise quotient equals' + str(answer))
 
 
-
+#puts resutls in output file and folder
 pickle.dump(answer,outfile)
 outfile.close()
